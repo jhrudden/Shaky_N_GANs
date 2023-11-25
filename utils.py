f93b19292822ec2de5d8a6e6307d4485d2765a2b
John Henry Rudden <johnhenryrudden@gmail.com>
2023-11-25 17:28:36 -0600

    Add <UNK> tokens to train data when token count is below UNK_THRESHOLD. This will help us handle unknown words during testing phase.

--- utils.py
+++ utils.py
@@ -1,8 +1,14 @@
+from typing import List
 import tensorflow_datasets as tfds
 import tensorflow as tf
 import numpy as np
 import nltk 
 import os
+from collections import Counter
+
+import ngram
+from embeddings import WordEmbeddingManager
+
 nltk.download('punkt')
 
 def load_decode_and_split_shakespeare(split):
@@ -23,12 +29,81 @@ def load_decode_and_split_shakespeare(split):
 
     return sentences
 
-def tokenize_sentences(sentences):
+def tokenize_sentences(sentences: List[str]) -> List[List[str]]:
+    """
+    Tokenizes a list of sentences into words.
+
+    Args:
+    sentences (List[str]): A list of sentences to tokenize.
+
+    Returns:
+    List[List[str]]: A list of tokenized sentences.
+    """
     # Tokenize sentences into words using NLTK
     return [nltk.word_tokenize(sentence) for sentence in sentences]
 
-def load_sentences(data_path: str):
+def load_sentences(data_path: str) -> List[str]:
+    """
+    Loads sentences from a file.
+
+    Args:
+    data_path (str): The file path from which to load sentences.
+
+    Returns:
+    List[str]: A list of sentences loaded from the file.
+    """
     assert os.path.isfile(data_path)
     with open(data_path, 'r') as f:
         sentences = f.readlines()
     return sentences
+
+def process_training_data(file_path: str) -> List[List[str]]:
+    """
+    Processes the training data by tokenizing sentences and replacing infrequent words with "<UNK>".
+
+    Args:
+    file_path (str): The path to the file containing the training data.
+
+    Returns:
+    List[List[str]]: A list of tokenized sentences with infrequent words replaced by "<UNK>".
+    """
+    sentences = load_sentences(file_path)
+    tokenized_sentences = tokenize_sentences(sentences)
+
+    # Count word occurrences
+    word_counts = Counter(word for sentence in tokenized_sentences for word in sentence)
+
+    # Replace rare words with UNK
+    processed_sentences = []
+    for sentence in tokenized_sentences:
+        processed_sentence = [word if word_counts[word] >= ngram.UNKNOWN_THRESHOLD else ngram.UNK for word in sentence]
+        processed_sentences.append(processed_sentence)
+
+    return processed_sentences
+
+def process_non_training_data(file_path: str, word_embedding_manager: WordEmbeddingManager) -> List[List[str]]:
+    """
+    Processes non-training data (such as validation or test data) by tokenizing sentences and 
+    replacing words not in the trained Word2Vec model's vocabulary with "<UNK>".
+
+    Args:
+    file_path (str): The path to the file containing the non-training data.
+    word_embedding_manager (WordEmbeddingManager): An instance of WordEmbeddingManager containing 
+    the trained Word2Vec model.
+
+    Returns:
+    List[List[str]]: A list of tokenized sentences with words not in the model's vocabulary replaced by "<UNK>".
+    """
+    sentences = load_sentences(file_path)
+    tokenized_sentences = tokenize_sentences(sentences)
+
+    # Retrieve the vocabulary from the Word2Vec model
+    trained_vocab = set(word_embedding_manager._model.wv.key_to_index.keys())
+
+    # Process sentences
+    processed_sentences = []
+    for sentence in tokenized_sentences:
+        processed_sentence = [word if word in trained_vocab else ngram.UNK for word in sentence]
+        processed_sentences.append(processed_sentence)
+
+    return processed_sentences
